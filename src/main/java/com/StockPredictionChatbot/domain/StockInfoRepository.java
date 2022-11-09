4202ee3e59b51979c93c6f90de06cfe4f87a360f
yeojinLee-dev <yeojinlee.dev@gmail.com>
2022-11-09 18:02:34 +0900

    fix : StockInfoRepository

--- src/main/java/com/StockPredictionChatbot/domain/StockInfoRepository.java
+++ src/main/java/com/StockPredictionChatbot/domain/StockInfoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockInfoRepository extends JpaRepository<Prediction, String> {
+public interface StockInfoRepository extends JpaRepository<StockInfo, String> {
     StockInfo findByStock(String stock);
 }
