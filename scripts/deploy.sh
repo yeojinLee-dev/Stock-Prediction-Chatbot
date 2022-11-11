cd6f430e522061d1c77eb1fedf6ca2f0fc2d5e74
yeojinLee-dev <yeojinlee.dev@gmail.com>
2022-11-11 16:40:24 +0900

    최종 배포

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REPOSITORY=/root/app/step2
+PROJECT_NAME=Stock-Prediction-Chatbot
+
+echo "> Build 파일 복사"
+cd $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl Stock-Prediction-Chatbot | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x %JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \-Dspring.config.location=classpath:/root/app/application.yml \ $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
